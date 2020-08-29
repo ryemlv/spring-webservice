a5d98fe4fe4a6d7232e6f89e1b0f2d9dd6d3d0f1
Ryan Cho <ryemlv@hotmail.com>
2020-08-29 23:35:17 +0900

    feature / post save api 기능 추가

--- src/test/java/com/ryancho/webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/ryancho/webservice/domain/posts/PostsRepositoryTest.java
@@ -15,14 +15,14 @@ public class PostsRepositoryTest {
     @Autowired
     PostsRepository postsRepository;
 
-    @After
-    public void cleanup() {
-        /**
-         이후 테스트 코드에 영향을 끼치지 않기 위해
-         테스트 메소드가 끝날때 마다 respository 전체 비우는 코드
-         **/
-        postsRepository.deleteAll();
-    }
+//    @After
+//    public void cleanup() {
+//        /**
+//         이후 테스트 코드에 영향을 끼치지 않기 위해
+//         테스트 메소드가 끝날때 마다 respository 전체 비우는 코드
+//         **/
+//        postsRepository.deleteAll();
+//    }
 
     @Test
     void contextLoads() {
