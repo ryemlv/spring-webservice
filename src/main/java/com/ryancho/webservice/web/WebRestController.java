3c0fa705bbbaeb530da9a50f1ca5d7f947f94b79
Ryan Cho <ryemlv@hotmail.com>
2020-08-29 19:06:21 +0900

    initial commit

---
+++ src/main/java/com/ryancho/webservice/web/WebRestController.java
@@ -0,0 +1,24 @@
+package com.ryancho.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "HelloWorld";
+    }
+}
+
+/*
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
+* */
